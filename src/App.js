84ce8e8995c1bac61f8f402a9b385ee4428886bf
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2019-01-07 16:16:34 +0000

    Add search bar & filters (No functionality)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
+          <div className="search-form">
+            <input type="text" placeholder="Search" />
+            <button>Search</button>
+          </div>
+          <div className="main-nav">
+            <ul>
+              <li><a href="#top">Cats</a></li>
+              <li><a href="#top">Dogs</a></li>
+              <li><a href="#top">Birds</a></li>
+            </ul>
+          </div>
       </div>
     );
   }
