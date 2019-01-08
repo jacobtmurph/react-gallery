4a299ce7785a213f74fc96a98daf2fc9de05fa41
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2019-01-08 14:55:58 +0000

    Add Gallery component w/ Fetch call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Nav from './Nav'
+import Nav from './Nav';
+import Gallery from './Gallery';
+import apiKey from './config';
+
+const flickrKey = apiKey;
 
 class App extends Component {
+
   render() {
     return (
       <div className="container">
           <Header title="Beflast Photographic Gallery" />
           <Nav but1="Belfast City" but2="Forests" but3="Beaches" />
+          <Gallery apiKey={ flickrKey } /> 
       </div>
     );
   }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
-    // render() {
-        
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: []
+        }
+    }
+    
+    componentDidMount() {
+        fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.props.apiKey}&tags=Belfast&per_page=24&format=json&nojsoncallback=1/`)
+        .then( response => response.json())
+        .then( responseData => this.setState({photos: responseData }) )
+        .catch(error => {
+            console.log("Error occured when fetching and/or parsing data", error);
+        });
+    }
+
+    render() {
+        console.log()
+        console.log(this.state.photos);
+        return (
+            <h1>Results</h1>
+        );
+    }
 }
+
+export default Gallery;
