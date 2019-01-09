081b7c77184792b1bbf20c37a4a390e4866d6dc6
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2019-01-09 17:05:27 +0000

    Add Gallery & Image components

--- src/App.js
+++ src/App.js
@@ -8,12 +8,28 @@ const flickrKey = apiKey;
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        flickr: []
+    }
+  }
+
+  componentDidMount() {
+      fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrKey}&tags=Belfast&per_page=24&format=json&nojsoncallback=1/`)
+      .then( response => response.json())
+      .then( responseData => this.setState({flickr: responseData }) )
+      .catch(error => {
+          console.log("Error occured when fetching and/or parsing data", error);
+      });
+  }
+
   render() {
     return (
       <div className="container">
           <Header title="Beflast Photographic Gallery" />
           <Nav but1="Belfast City" but2="Forests" but3="Beaches" />
-          <Gallery apiKey={ flickrKey } /> 
+          <Gallery pics= { this.state.flickr.photoset } /> 
       </div>
     );
   }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Image from './Image';
 
-class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            photos: []
-        }
-    }
+const Gallery = props => {
     
-    componentDidMount() {
-        fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.props.apiKey}&tags=Belfast&per_page=24&format=json&nojsoncallback=1/`)
-        .then( response => response.json())
-        .then( responseData => this.setState({photos: responseData }) )
-        .catch(error => {
-            console.log("Error occured when fetching and/or parsing data", error);
-        });
-    }
+    const results = props.pics;
+    let images = results.map(image =>  
+        <Image src={`https://farm{image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} />
+    );
 
-    render() {
-        console.log()
-        console.log(this.state.photos);
-        return (
-            <h1>Results</h1>
-        );
-    }
-}
+    return (
+        <h1>Results</h1>
+        <div className="photo-container">
+            <ul>
+                { images }
+            </ul>
+        </div>
+    )
 
+};
 export default Gallery;
